728b95dccab6192a397a89408bf54df50e630c28
Moemen <moemenhussein44@gmail.com>
2022-10-23 23:21:55 +0200

    added variables to ArticleDetail model

--- src/main/java/nl/Groep13/OrderHandler/model/ArticleDetail.java
+++ src/main/java/nl/Groep13/OrderHandler/model/ArticleDetail.java
@@ -1,4 +1,37 @@
 package nl.Groep13.OrderHandler.model;
 
 public class ArticleDetail {
+    private int eancode;
+    private String productGroup;
+    private String supplier;
+
+    public ArticleDetail(int eancode, String productGroup, String supplier) {
+        this.eancode = eancode;
+        this.productGroup = productGroup;
+        this.supplier = supplier;
+    }
+
+    public int getEancode() {
+        return eancode;
+    }
+
+    public void setEancode(int eancode) {
+        this.eancode = eancode;
+    }
+
+    public String getProductGroup() {
+        return productGroup;
+    }
+
+    public void setProductGroup(String productGroup) {
+        this.productGroup = productGroup;
+    }
+
+    public String getSupplier() {
+        return supplier;
+    }
+
+    public void setSupplier(String supplier) {
+        this.supplier = supplier;
+    }
 }
