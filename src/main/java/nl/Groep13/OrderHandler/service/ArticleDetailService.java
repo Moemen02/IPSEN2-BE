4e9947509a19bbbc02f3ac70e689a380e321b0c7
Moemen <moemenhussein44@gmail.com>
2022-10-26 22:47:41 +0200

    added ArticleDetailService & ArticlePriceService

---
+++ src/main/java/nl/Groep13/OrderHandler/service/ArticleDetailService.java
@@ -0,0 +1,10 @@
+package nl.Groep13.OrderHandler.service;
+
+public class ArticleDetailService {
+
+    private final ArticleService articleService;
+
+    public ArticleDetailService(ArticleService articleService) {
+        this.articleService = articleService;
+    }
+}
---
+++ src/main/java/nl/Groep13/OrderHandler/service/ArticlePriceService.java
@@ -0,0 +1,10 @@
+package nl.Groep13.OrderHandler.service;
+
+public class ArticlePriceService {
+
+    private final ArticlePriceService articlePriceService;
+
+    public ArticlePriceService(ArticlePriceService articlePriceService) {
+        this.articlePriceService = articlePriceService;
+    }
+}
