a415b7e0fad1c0d69def31a7b0f704199b44f490
Bokko2000 <matthewdeboer16@gmail.com>
2022-11-02 18:37:22 +0100

    Update: label controller now has response body for label data.

--- src/main/java/nl/Groep13/OrderHandler/controller/LabelController.java
+++ src/main/java/nl/Groep13/OrderHandler/controller/LabelController.java
@@ -2,21 +2,32 @@ package nl.Groep13.OrderHandler.controller;
 
 import com.google.gson.Gson;
 import nl.Groep13.OrderHandler.service.LabelService;
+import nl.Groep13.OrderHandler.service.MakeExcelService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.crossstore.ChangeSetPersister;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.FileNotFoundException;
+import java.util.HashMap;
 
 @RestController
 @RequestMapping(value = "/api/label")
 public class LabelController {
    private final LabelService labelService;
+   private MakeExcelService makeExcelService;
 
     Gson gson = new Gson();
 
     @Autowired
-    public LabelController(LabelService labelService) {
+    public LabelController(LabelService labelService, MakeExcelService makeExcelService) {
         this.labelService = labelService;
+        this.makeExcelService = makeExcelService;
+    }
+
+    @GetMapping(value = "/{id}")
+    @ResponseBody
+    public HashMap<String,String> getLabelData(@PathVariable Long id) throws FileNotFoundException, ChangeSetPersister.NotFoundException {
+        makeExcelService.getLabelData(id);
     }
 
 //    @GetMapping
