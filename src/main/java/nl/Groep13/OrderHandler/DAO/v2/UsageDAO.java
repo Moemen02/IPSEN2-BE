1c6499e8b0abef4dc8f9444b432737ba8b4ae26b
Trainwheel <s1130541@student.hsleiden.nl>
2022-12-14 00:44:40 +0100

    PATCH: Usage Model now has a working constructor and the table name has been changed due to it being a keyword for MySQL which interferes with using GET. PATCH: UsageDAO has a functional getAll function and GetById. PATCH: UsageController now has been connected with the functional UsageDAO. ADD: UsageControllerTest file to later test the created functions. REMOVE: UsageInterface has been removed from UsageController as constructor argument.

--- src/main/java/nl/Groep13/OrderHandler/DAO/v2/UsageDAO.java
+++ src/main/java/nl/Groep13/OrderHandler/DAO/v2/UsageDAO.java
@@ -2,24 +2,38 @@ package nl.Groep13.OrderHandler.DAO.v2;
 
 import nl.Groep13.OrderHandler.interfaces.UsageInterface;
 import nl.Groep13.OrderHandler.model.v2.Usage;
+import nl.Groep13.OrderHandler.repository.v2.UsageRepository;
+import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+import java.util.Optional;
 
 @Component
-public class UsageDAO implements UsageInterface {
-    @Override
+public class UsageDAO {
+
+    private final UsageRepository usageRepository;
+
+    public UsageDAO(final UsageRepository usageRepository) {
+        this.usageRepository = usageRepository;
+    }
+
+
     public List<Usage> getAllUsages() {
-        return null;
+        return usageRepository.findAll();
     }
 
-    @Override
-    public Usage getUsageById(Long id) {
-        return null;
+
+    public Usage getUsageById(Long id) throws ChangeSetPersister.NotFoundException {
+        Optional<Usage> usage = usageRepository.findById(id);
+        if (usage.isPresent()) {
+            return usage.get();
+        }
+        throw new ChangeSetPersister.NotFoundException();
     }
 
-    @Override
-    public Usage addUsage(Usage usage) {
-        return null;
+
+    public Usage addUsage(final Usage usage) {
+        return this.usageRepository.save(usage);
     }
 }
---
+++ src/test/java/nl/Groep13/OrderHandler/controller/v2/UsageControllerTest.java
@@ -0,0 +1,13 @@
+package nl.Groep13.OrderHandler.controller.v2;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ExtendWith(SpringExtension.class)
+public class UsageControllerTest {
+
+}
