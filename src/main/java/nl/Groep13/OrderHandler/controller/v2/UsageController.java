f18dd04b2cd694f11c95c3e8ac23c2c9c2cd0c23
Moemen <moemenhussein44@gmail.com>
2023-01-03 02:24:23 +0100

    FEAT: returns usageID based on pattern length

--- src/main/java/nl/Groep13/OrderHandler/controller/v2/UsageController.java
+++ src/main/java/nl/Groep13/OrderHandler/controller/v2/UsageController.java
@@ -1,6 +1,7 @@
 package nl.Groep13.OrderHandler.controller.v2;
 
 import nl.Groep13.OrderHandler.DAO.v2.UsageDAO;
+import nl.Groep13.OrderHandler.model.v2.ArticleData;
 import nl.Groep13.OrderHandler.model.v2.Usage;
 import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.http.HttpStatus;
@@ -35,6 +36,39 @@ public class UsageController{
         } catch (ChangeSetPersister.NotFoundException e) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
         }
+    }
+
+    public long setUsageType(ArticleData articleData){
+        List<Usage> usageList = this.usageDAO.getAllUsages();
+        Float patternLength = articleData.getPatternLength();
+        final long[] returningUsageID = {0L};
+
+        //is..when is when article is bigger then value then it gets a usage type
+        Float isSafedWhen = 2F;
+        Float isRetourWhen = 5F;
+
+        String usageType;
+
+        if (patternLength <= isSafedWhen){
+            usageType = "AFVAL";
+        } else if (patternLength <= isRetourWhen) {
+            usageType = "WASTE";
+        } else {
+            usageType = "RETOUR";
+        }
+
+        usageList.forEach(usage -> {
+            if (usage.getType_usage().equals(usageType)){
+                returningUsageID[0] = usage.getId();
+            }
+        });
+
+        return returningUsageID[0];
+    }
+
+    public String getUsageType(Long id) throws ChangeSetPersister.NotFoundException {
+        Usage usage = this.usageDAO.getUsageById(id);
+        return usage.getType_usage();
     }
 
     @PostMapping
