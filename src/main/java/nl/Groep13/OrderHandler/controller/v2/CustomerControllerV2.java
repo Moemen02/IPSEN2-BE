0ed373c1b1602ae8c0b38d981184d87cf313bd28
Mohammed <s1136968@student.hsleiden.nl>
2023-01-11 13:21:56 +0100

    feat: able to receive all data in order page front end except location

--- src/main/java/nl/Groep13/OrderHandler/controller/v2/CustomerControllerV2.java
+++ src/main/java/nl/Groep13/OrderHandler/controller/v2/CustomerControllerV2.java
@@ -62,25 +62,25 @@ public class CustomerControllerV2 {
      * @param customer - this is a customer
      * @returns a updated model of customer
      */
-    @PutMapping(value = "/{id}")
-    @ResponseBody
-    public Optional<CustomerV2> updateCustomer(@PathVariable Long id, @RequestBody Map<String, String> customer){
-        String customerToJson = gson.toJson(customer);
-        CustomerV2 newCustomer = gson.fromJson(customerToJson, CustomerV2.class);
+//    @PutMapping(value = "/{id}")
+//    @ResponseBody
+//    public Optional<CustomerV2> updateCustomer(@PathVariable Long id, @RequestBody Map<String, String> customer){
+//        String customerToJson = gson.toJson(customer);
+//        CustomerV2 newCustomer = gson.fromJson(customerToJson, CustomerV2.class);
+//
+//        return this.customerService.updateCustomer(id, Optional.of(newCustomer));
+//
+//    }
 
-        return this.customerService.updateCustomer(id, Optional.of(newCustomer));
-
-    }
-
-    @DeleteMapping
-    public ResponseEntity<Boolean> deleteCustomer(@PathVariable final Long id) {
-        try {
-            customerService.deleteCustomer(id);
-        } catch (ChangeSetPersister.NotFoundException e) {
-            return ResponseEntity.ok(false);
-        }
-        return ResponseEntity.ok(true);
-    }
+//    @DeleteMapping
+//    public ResponseEntity<Boolean> deleteCustomer(@PathVariable final Long id) {
+//        try {
+//            customerService.deleteCustomer(id);
+//        } catch (ChangeSetPersister.NotFoundException e) {
+//            return ResponseEntity.ok(false);
+//        }
+//        return ResponseEntity.ok(true);
+//    }
 
     @PostMapping
     public ResponseEntity<Boolean> addCustomer(@RequestParam Map<String, String> customer) {
