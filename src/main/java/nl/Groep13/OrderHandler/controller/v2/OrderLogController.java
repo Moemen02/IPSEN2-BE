36b0ee2afe1fc36c32455f663d63bc18748ee791
Moemen <moemenhussein44@gmail.com>
2023-01-25 22:07:50 +0100

    FEAT: creation of order log

--- src/main/java/nl/Groep13/OrderHandler/controller/v2/OrderLogController.java
+++ src/main/java/nl/Groep13/OrderHandler/controller/v2/OrderLogController.java
@@ -1,23 +1,37 @@
 package nl.Groep13.OrderHandler.controller.v2;
 
+import nl.Groep13.OrderHandler.controller.UserController;
 import nl.Groep13.OrderHandler.interfaces.OrderLogInterface;
+import nl.Groep13.OrderHandler.model.User;
+import nl.Groep13.OrderHandler.model.v2.ArticleOrder;
+import nl.Groep13.OrderHandler.model.v2.OrderLog;
+import org.hibernate.criterion.Order;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/api/v2/order/log")
 public class OrderLogController {
 
     private final OrderLogInterface orderLogInterface;
+    private final UserController userController;
 
-    public OrderLogController(OrderLogInterface orderLogInterface) {
+    public OrderLogController(OrderLogInterface orderLogInterface, UserController userController) {
         this.orderLogInterface = orderLogInterface;
+        this.userController = userController;
     }
 
     @GetMapping
     public ResponseEntity<String> getOrderLog(){
         return ResponseEntity.ok().body("dit moet een order log item zijn");
+    }
+
+    @PostMapping
+    public ResponseEntity<OrderLog> saveOrderLog(@RequestBody ArticleOrder order, Authentication authentication){
+        User user = this.userController.getAuthUser(authentication);
+        System.out.println(order);
+        OrderLog orderLog = new OrderLog(order.getId(), user.getId());
+        return ResponseEntity.ok(this.orderLogInterface.saveOrder(orderLog));
     }
 }
--- src/main/java/nl/Groep13/OrderHandler/model/v2/OrderLog.java
+++ src/main/java/nl/Groep13/OrderHandler/model/v2/OrderLog.java
@@ -1,20 +1,27 @@
 package nl.Groep13.OrderHandler.model.v2;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.criterion.Order;
 
 import javax.persistence.*;
 
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor
 @Table(name = "order_log")
 public class OrderLog {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private Long orderId;
-    private Long userId;
+    private Long orderID;
+    private Long userID;
 
+    public OrderLog(Long orderId, Long userId) {
+        this.orderID = orderId;
+        this.userID = userId;
+    }
 }
